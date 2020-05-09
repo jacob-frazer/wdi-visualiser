467656502dd251eb041290a9b2cef6153e132402
Jacob Frazer <jakefrazer1997@googlemail.com>
2020-05-09 18:41:10 +0100

    Wired up front end to send the ML params

--- node/index.js
+++ node/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.listen(3000, () => {
-    console.log("Server running on port 3000")
+// setting up cors to allow cross origin requests etc
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.listen(4000, () => {
+    console.log("Server running on port 4000")
 })
 
 const test_data = { 
@@ -20,6 +26,26 @@ app.get('/mlSubmit', (req, res) => {
 
     // put the details from the post request about the ML model req.query.firstname
     var pythonProcess = spawn('python',["../python/ml_modeller.py", JSON.stringify(test_data)] ); 
+  
+    // Takes stdout data from script which executed 
+    // with arguments and send this data to res object 
+    pythonProcess.stdout.on('data', (data) => { 
+        // return the data to front end to display
+        data = data.toString('utf-8')
+
+        res.send(JSON.parse(data))
+    } ) 
+})
+
+app.post('/mlSubmit', (req, res) => {
+    // Front end will post the ML details to this url - from which it will call the python ML to calculate it
+    let spawn = require("child_process").spawn;
+
+    console.log("The request we received from the front end is:")
+    console.log(req.body)
+
+    // put the details from the post request about the ML model req.query.firstname
+    var pythonProcess = spawn('python',["../python/ml_modeller.py", JSON.stringify(req.body)] ); 
   
     // Takes stdout data from script which executed 
     // with arguments and send this data to res object 
--- react/wdi-visualiser/src/App.js
+++ react/wdi-visualiser/src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+
+class App extends Component {
+
+  // for now bind our example to the state
+  state = {
+    ml_params: {
+      "ml_type": "lin_regression", 
+      "dep_var": "NY.ADJ.NNTY.CD", 
+      "indep_vars": ["NY.ADJ.AEDU.GN.ZS", "SE.SEC.UNER.LO.ZS"],
+      "countries": ["ARB", "UKR", "USA", "GBR", "BGR", "SPA", "NOR", "FRO", "MEX"],
+      "start_year": 1990,
+      "end_year": 2019 
+    },
+    name: "Jake"
+  };
+
+  handleClick () {
+    axios.post('http://localhost:4000/mlSubmit', this.state.ml_params)
+    .then( (response) => {
+      console.log(response);
+    }, (error) => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Welcome to ML builder of the World Development Index dataset
+          </p>
+          <p>
+            The current request that will be built is: 
+            <br/>
+            {this.state.ml_params.ml_type}
+          </p>
+          <button className='button' onClick={this.handleClick.bind(this)}>Generate Machine Learning Model!</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
