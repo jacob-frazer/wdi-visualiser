cb3e6c801f0918c39b5aefad64af0374486eb317
Jacob Frazer <jakefrazer1997@googlemail.com>
2021-07-17 19:14:52 +0100

    Adding run file and build version

---
+++ react/wdi-visualiser/build/precache-manifest.555377191d5f79d776f70e7f9d86a7d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "004668949bb93b5c4b6cede968711ed9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8151d6e95709dbb28069",
+    "url": "/static/css/main.e08610ba.chunk.css"
+  },
+  {
+    "revision": "ad9034c161f7ecf7b54b",
+    "url": "/static/js/2.1358adeb.chunk.js"
+  },
+  {
+    "revision": "023f699be63df210078dba5b2b0c7ec6",
+    "url": "/static/js/2.1358adeb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8151d6e95709dbb28069",
+    "url": "/static/js/main.e6127439.chunk.js"
+  },
+  {
+    "revision": "8d586706e15cbb8b698d",
+    "url": "/static/js/runtime-main.1e0f61c5.js"
+  },
+  {
+    "revision": "a6d703aca971818223e2dcb7f8844da7",
+    "url": "/static/media/world_bank.a6d703ac.png"
+  }
+]);
---
+++ run.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# script must be ran from WDI main dir currently
+
+# start mongodb -- v important that this is first!
+pm2 start mongod --name database --watch
+
+# start python listener
+cd python
+pm2 start .\api.py --name python-listener --watch --interpreter=python
+cd ..
+
+# start the node back end as service -- might be able to pass the node into path rather than cd in and out
+cd node
+pm2 --name web_backend start .\index.js --watch
+cd ..
+
+# start react front end
+cd react/wdi-visualiser
+pm2 serve .\build\ 2000 --spa --watch --name frontend
+cd ../..
+
